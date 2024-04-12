87a14edf0dd5ee32b6bce777a6a10b9c18a5bcab
manas <bagrimanasbir@gmail.com>
2024-04-11 19:51:47 -0700

    added router + tables

---
+++ ui/src/App.tsx
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <>
+      <h1>manas!</h1>
+      <h2>dev @ <a href="https://protecc.xyz/">Protecc Labs</a></h2>
+
+      <p>interested in crypto, enlightenment, and <a href="https://youtu.be/floMqK_yHf8?t=121">jungkook's dance solo in mic drop at MAMA</a></p>
+      <p>familiar with solidity, huff, rust, js, ts, python, c++, evm</p>
+      <p>curr building in stealth</p>
+      <p>
+        <a href="https://github.com/manasbir/">github</a>
+      </p>
+      <p>
+        <a href="https://twitter.com/manazane/">twitter</a>
+      </p>
+      <p>manas/manasbir/manazane</p>
+    </>
+  )
+}
+
+export default App
